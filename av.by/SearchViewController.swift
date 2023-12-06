f7343512460835bcf97b4e4a5299ea5d443320c9
SnakCat <snakkdss@gmail.com>
2023-12-06 18:58:45 +0300

    create TableView

--- av.by/SearchViewController.swift
+++ av.by/SearchViewController.swift
@@ -1,14 +1,43 @@
 import UIKit
 
 final class SearchViewController: UIViewController {
+    // MARK: - private propertis
+    private let tableView = UITableView()
    
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupConstraints()
        
+    }
+    // MARK: - helpers methods
+    private func setupConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
     
 
+}
+    // MARK: - extension
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
 }
   
     
